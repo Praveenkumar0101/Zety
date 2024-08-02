aa8027dde5247d560798978d4ac97f41ec2a63ee
Praveenkumar0101 <Praveenkumar.vancharla@gmail.com>
2024-08-02 16:39:13 +0530

    update

--- src/logos.js
+++ src/logos.js
@@ -4,31 +4,35 @@ import './CompanyLinks.css';  // Import your CSS file
 const CompanyLinks = () => {
   return (
     <div className="company-container">
+      <div className='hired'>
+      <h3>Our customers have been <br/> hired by:*</h3>
+      </div>
       <a href="https://www.microsoft.com" target="_blank" rel="noopener noreferrer" className="company-link">
-        <img src="https://th.bing.com/th/id/OIP.olOnH8y5yZLeWr0g27wxbgHaE4?w=728&h=480&rs=1&pid=ImgDetMain" alt="Microsoft" className="company-logo" />
-        <div className="company-name">Microsoft</div>
+        <img src="https://th.bing.com/th/id/OIP.kkNY5vBdK3HMrpTmZ6qA0gHaBl?pid=ImgDet&w=168&h=35.88365172314486&c=7&dpr=1.3" alt="Microsoft" className="company-logo" />
+      
       </a>
       <a href="https://www.amazon.com" target="_blank" rel="noopener noreferrer" className="company-link">
-        <img src="https://image.freepik.com/free-icon/amazon-logo_318-55469.jpg" alt="Amazon" className="company-logo" />
-        <div className="company-name">Amazon</div>
+        <img src="https://th.bing.com/th/id/OIP.StQyiTC4ZdBIwJ3hizFYpwHaCO?pid=ImgDet&w=168&h=50.434355828220866&c=7&dpr=1.3" alt="Amazon" className="company-logo" />
+      
       </a>
       <a href="https://www.att.com" target="_blank" rel="noopener noreferrer" className="company-link">
-        <img src="https://seeklogo.com/images/A/AT_and_T-logo-7691ACC057-seeklogo.com.png" alt="AT&T" className="company-logo" />
-        <div className="company-name">AT&T</div>
+        <img src="https://th.bing.com/th/id/OIP.UPIzG8UfM36-bX_bN_iLgwHaDC?pid=ImgDet&w=168&h=94.5&c=7&dpr=1.3" alt="AT&T" className="company-logo" />
+       
       </a>
-      <a href="https://www.google.com" target="_blank" rel="noopener noreferrer" className="company-link">
-        <img src="path-to-logo/google.png" alt="Google" className="company-logo" />
-        <div className="company-name">Google</div>
+      <a href="https://www.walmart.com/" target="_blank" rel="noopener noreferrer" className="company-link">
+        <img src="https://th.bing.com/th/id/OIP.hdWAqx3qRjCkkIdZiHW0pwHaCI?pid=ImgDet&w=168&h=44.21052631578947&c=7&dpr=1.3" alt="Walmart" className="company-logo" />
       </a>
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="company-link">
-        <img src="path-to-logo/facebook.png" alt="Facebook" className="company-logo" />
-        <div className="company-name">Facebook</div>
+      <a href="https://www.fedex.com/" target="_blank" rel="noopener noreferrer" className="company-link">
+        <img src="https://logohistory.net/wp-content/uploads/2023/05/FedEx-Symbol.png" alt="Fedex" className="company-logo" />
+      
       </a>
-      <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="company-link">
-        <img src="path-to-logo/twitter.png" alt="Twitter" className="company-logo" />
-        <div className="company-name">Twitter</div>
+      <a href="https://www.orange.com/" target="_blank" rel="noopener noreferrer" className="company-link">
+        <img src="https://th.bing.com/th/id/OIP.eqDP-aXGorg7eI1CiAvRIgHaCA?rs=1&pid=ImgDetMain" alt="Orange" className="company-logo" />
+       
       </a>
+      
     </div>
+    
   );
 };
 
--- src/navbar.js
+++ src/navbar.js
@@ -30,32 +30,73 @@ function Navbar() {
         <div className="dropdown" onMouseEnter={() => handleMouseEnter('about')} onMouseLeave={handleMouseLeave}>
           <button className="dropbtn">About <i className={`fa ${openDropdown === 'about' ? 'fa-caret-up' : 'fa-caret-down'}`}></i></button>
           <div className={`dropdown-content ${openDropdown === 'about' ? 'show' : ''}`}>
-          <a href=''><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /></a>
+          <a href=''><img src='https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-2/512/sms_text_protocol_message-1024.png' alt="Tools Icon" /></a>
           <div className='text'>
           
           <h3>About</h3>
           <p>Find out more about zety and  it's <br/> <span style={{marginLeft:"12px"}}> career experts.Get the job you want</span></p>
            
           </div>
-          <div className='about'>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" />Pricing</a>
+          {/* <div className='about'>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.LQEJATS7RP7HIZkQmgr2HgHaHa?rs=1&pid=ImgDetMain' alt="Tools Icon" />Pricing</a>
             <p>Explore our Pricing options</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" />Contact</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.UvqagJOkjSpzXYZcT5K-lQHaHF?rs=1&pid=ImgDetMain' alt="Tools Icon" />Contact</a>
             <p>Our customer servce team is ready to help</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" />Editorial Guidelines</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.GWcyurPnyjxuij5yXRjxNAHaHN?rs=1&pid=ImgDetMain' alt="Tools Icon" />Editorial Guidelines</a>
             <p>Learn how our experts create our content</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" />Media Mentions</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.3iMERZcKR0bDNyz4aARCyQHaE0?rs=1&pid=ImgDetMain' alt="Tools Icon" />Media Mentions</a>
             <p>Discover where our work has been featured in</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" />Press Page</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.KP_wS6-4sBh5BkxXUhDOSQHaHa?rs=1&pid=ImgDetMain' alt="Tools Icon" />Press Page</a>
             <p>Read orginal data insides to boost your reporting</p>
           </div>
         </div>
+        </div> */}
+
+
+<div className='about'>
+          <a href="#">
+            <img src='https://th.bing.com/th/id/OIP.LQEJATS7RP7HIZkQmgr2HgHaHa?rs=1&pid=ImgDetMain' alt="Tools Icon" />
+            <span>
+              Pricing
+              <p>Explore our Pricing options</p>
+            </span>
+          </a>
+          <a href="#">
+            <img src='https://th.bing.com/th/id/OIP.UvqagJOkjSpzXYZcT5K-lQHaHF?rs=1&pid=ImgDetMain' alt="Tools Icon" />
+            <span>
+              Contact
+              <p>Our customer service team is ready to help</p>
+            </span>
+          </a>
+          <a href="#">
+            <img src='https://th.bing.com/th/id/OIP.GWcyurPnyjxuij5yXRjxNAHaHN?rs=1&pid=ImgDetMain' alt="Tools Icon" />
+            <span>
+              Editorial Guidelines
+              <p>Learn how our experts create our content</p>
+            </span>
+          </a>
+          <a href="#">
+            <img src='https://th.bing.com/th/id/OIP.3iMERZcKR0bDNyz4aARCyQHaE0?rs=1&pid=ImgDetMain' alt="Tools Icon" />
+            <span>
+              Media Mentions
+              <p>Discover where our work has been featured in</p>
+            </span>
+          </a>
+          <a href="#">
+            <img src='https://th.bing.com/th/id/OIP.KP_wS6-4sBh5BkxXUhDOSQHaHa?rs=1&pid=ImgDetMain' alt="Tools Icon" />
+            <span>
+              Press Page
+              <p>Read original data insights to boost your reporting</p>
+            </span>
+          </a>
         </div>
-         
+      </div>
+    </div>
+
         <div className="dropdown" onMouseEnter={() => handleMouseEnter('career-blog')} onMouseLeave={handleMouseLeave}>
           <button className="dropbtn">Career Blog <i className={`fa ${openDropdown === 'career-blog' ? 'fa-caret-up' : 'fa-caret-down'}`}></i></button>
           <div className={`dropdown-content ${openDropdown === 'career-blog' ? 'show' : ''}`}>
-          <a href=''><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /></a>
+          <a href=''><img src='https://th.bing.com/th/id/OIP.SGFg3Ej3d0YmQVCfKlBZPwHaI_?rs=1&pid=ImgDetMain' alt="Tools Icon" /></a>
           <div className='text'>
           
           <h3>See all posts</h3>
@@ -64,19 +105,19 @@ function Navbar() {
           </div>
 
           <div className='career-blog'>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /> Job Search </a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.SgTGPQP4T7hUJ5cigKpl2wHaHa?rs=1&pid=ImgDetMain' alt="Tools Icon" /> Job Search </a>
            <p> Learn how to find the right job and get it</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /> Job Interviews</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.Zjw87n1xnFlnnIq83VfavAAAAA?rs=1&pid=ImgDetMain' alt="Tools Icon" /> Job Interviews</a>
             <p>Prepare for any interview and ace it</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /> Career Advice</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.lszbfCy70c-L7WqLg1lyOwHaHa?rs=1&pid=ImgDetMain' alt="Tools Icon" /> Career Advice</a>
             <p>Improve your career with expert tips and strategies</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /> Resume Help</a>
+            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/9740/9740095.png' alt="Tools Icon" /> Resume Help</a>
             <p>Improve your resume with help from expert guides</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /> CV Help</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.4p-rOOfacPN8VOIN9jLlAAHaHa?w=1920&h=1920&rs=1&pid=ImgDetMain' alt="Tools Icon" /> CV Help</a>
             <p>Improve your cv with help from expert guides</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /> Cover Letter Help</a>
+            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/7502/7502750.png' alt="Tools Icon" /> Cover Letter Help</a>
             <p>Boost your chances of having your resume read with our help</p>
-            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /> Frequently Asked Questions</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.wZDLq8JOZv-kbqUYJxSoXgHaHa?w=512&h=512&rs=1&pid=ImgDetMain' alt="Tools Icon" /> Frequently Asked Questions</a>
             <p>Get answers to Frequently asked questions about resumes, Zety, and more</p>
           </div>
         </div>
@@ -85,7 +126,7 @@ function Navbar() {
         <div className="dropdown" onMouseEnter={() => handleMouseEnter('cover-letter')} onMouseLeave={handleMouseLeave}>
           <button className="dropbtn">Cover Letter <i className={`fa ${openDropdown === 'cover-letter' ? 'fa-caret-up' : 'fa-caret-down'}`}></i></button>
           <div className={`dropdown-content ${openDropdown === 'cover-letter' ? 'show' : ''}`}>
-          <a href=''><img src='https://cdn-icons-png.flaticon.com/512/1876/1876460.png' alt="Tools Icon" /></a>
+          <a href=''><img src='https://cdn-icons-png.flaticon.com/512/8212/8212266.png' alt="Tools Icon" /></a>
           <div className='text'>
           
           <h3>cover letter Builder</h3>
@@ -93,15 +134,15 @@ function Navbar() {
            
           </div>
           <div className='cover-letter'>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>Cover Letter Template</a>
+            <a href="#"><img src='https://workwriteservices.b-cdn.net/wp-content/uploads/2021/08/Cover-Letters-Icon-Green@2x.png'/>Cover Letter Template</a>
             <p>Find the perfect cover letter</p>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>Cover Letter Examples</a>
+            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/10577/10577960.png'/>Cover Letter Examples</a>
             <p>see perfect cover letter samples that gets jobs</p>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>Cover Letter Format</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.D8SIET8TZsQgYAIEvdUArwHaHY?w=1205&h=1200&rs=1&pid=ImgDetMain'/>Cover Letter Format</a>
               <p>pick the right format for your situation</p>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>How to Write Cover Letter</a>
+            <a href="#"><img src='https://th.bing.com/th/id/OIP.RJPFwSM0QMQyaaprABs7OAHaHa?pid=ImgDet&w=179&h=179&c=7&dpr=1.3'/>How to Write Cover Letter</a>
              <p>Learn how to write a  cover letter that gets jobs</p>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>Cover Letter Help</a>
+            <a href="#"><img src='https://cdn-icons-png.flaticon.com/512/7921/7921397.png'/>Cover Letter Help</a>
             <p>Boost your chances of having your resume read with our help</p>
             <div className="cover-letter-button">
         <a href="#" className="button">Create Your Cover letter Now</a>
@@ -121,15 +162,15 @@ function Navbar() {
            
           </div>
           <div className='cv'>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>CV Templates</a>
+            <a href="#"><img src='https://i0.wp.com/www.titanui.com/wp-content/uploads/2016/03/05/Minimal-Clean-Resume-CV-Template-With-Icons-PSD.jpg?resize=660%2C487'/>CV Templates</a>
             <p>Find the  perfeect cv template</p>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>CV Examples</a>
+            <a href="#"><img src='https://e7.pngegg.com/pngimages/135/434/png-clipart-computer-icons-curriculum-vitae-resume-icon-cv-template-blue.png'/>CV Examples</a>
             <p>See perfect resume samples.that get jobs</p>
-            <a href=''><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>CV format</a>
+            <a href=''><img src='https://th.bing.com/th/id/OIP.f437g3CFf0fh9fedcqEzXwHaHa?rs=1&pid=ImgDetMain'/>CV format</a>
             <p>Pick the right format for your situation</p>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>How to Write CV</a>
+            <a href="#"><img src='https://thumbs.dreamstime.com/b/cv-writing-job-application-resume-icon-vector-graphics-various-use-187075484.jpg'/>How to Write CV</a>
             <p>Learn how to write a cv that gets interviews</p>
-            <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg'/>CV Help</a>
+            <a href="#"><img src='https://p1.hiclipart.com/preview/457/92/957/resume-icon-help-and-support-icon-account-icon-line-electric-blue-symbol-logo-png-clipart.jpg'/>CV Help</a>
             <p>pick the right resume format for your situation</p>
             <div className="cv-button">
         <a href="#" className="button">Create Your CV now</a>
@@ -149,17 +190,17 @@ function Navbar() {
       <p>Create a resume in 5 minutes. <br/> <span style={{marginLeft:"15px"}}>Get the job you want</span></p>
     </div>
     <div className='Resume'>
-      <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg' alt="Resume Templates"/> Resume Templates</a>
+      <a href="#"><img src='https://th.bing.com/th/id/OIP._iEFVz20Vq8dixqJd4oergHaHa?rs=1&pid=ImgDetMain' alt="Resume Templates"/> Resume Templates</a>
       <p>Find the perfect resume template</p>
-      <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg' alt="Resume Examples"/> Resume Examples</a>
+      <a href="#"><img src='https://cdn3.iconfinder.com/data/icons/online-job-search/500/vab1175_17_resume_job_paper_isometric-512.png' alt="Resume Examples"/> Resume Examples</a>
       <p>See perfect resume samples that get jobs</p>
-      <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg' alt="Resume Format"/> Resume Format</a>
+      <a href="#"><img src='https://www.clipartkey.com/mpngs/m/51-510700_resume-and-cover-letter-icon.png' alt="Resume Format"/> Resume Format</a>
       <p>Pick the right resume format for your situation</p>
-      <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg' alt="How to Write a Resume"/> How to Write a Resume</a>
+      <a href="#"><img src='https://th.bing.com/th/id/OIP.5jUdAXiJqStCqNn8NcGIoQHaHR?rs=1&pid=ImgDetMain' alt="How to Write a Resume"/> How to Write a Resume</a>
       <p>Learn how to make a resume that gets interviews</p>
-      <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg' alt="Resume Checker"/> Resume Checker</a>
+      <a href="#"><img src='https://th.bing.com/th/id/OIP.QGu3rvpiDwqEnPUE8CVGIQHaHa?w=1000&h=1000&rs=1&pid=ImgDetMain' alt="Resume Checker"/> Resume Checker</a>
       <p>Get your resume checked and scored with one click</p>
-      <a href="#"><img src='https://icon-library.com/images/resume-icon/resume-icon-16.jpg' alt="Resume Helper"/> Resume Helper</a>
+      <a href="#"><img src='https://img.freepik.com/premium-vector/resume-icon-set-searching-job-selecting-staff-searching-professional-staff-analyzing-personnel-resume-resume-form-concept-employment-vector-isolated-white-background-eps-10_399089-1024.jpg?w=1380' alt="Resume Helper"/> Resume Helper</a>
       <p>Improve your resume with help from expert guides</p>
       <div className="resume-button">
         <a href="#" className="button">Create Your Resume Now</a>
